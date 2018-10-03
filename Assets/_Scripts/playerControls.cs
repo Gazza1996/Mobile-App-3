b92adde195b687c909bce49a65e401ee3c5793a2
Gary Mannion <gkmannion@hotmail.com>
2018-10-03 01:51:27 +0100

    PlayerIdle and Run movement added to game

---
+++ Assets/_Scripts/playerControls.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControls : MonoBehaviour {
+
+    // variables
+    public float maxSpeed;
+    
+    Rigidbody2D RB;
+    Animator myAnim;
+    bool facingRight;
+
+	// Use this for initialization
+	void Start () {
+        RB = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
+
+        facingRight = true;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float move = Input.GetAxis("Horizontal");
+        myAnim.SetFloat("speed", Mathf.Abs(move));
+
+        RB.velocity = new Vector2(move * maxSpeed, RB.velocity.y);
+
+        if (move > 0 && !facingRight)
+        {
+            flip();
+        }else if(move < 0 && facingRight){
+            flip();
+        }
+   
+    }
+
+    void flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
