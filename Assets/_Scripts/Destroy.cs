f492c9b636bd8f8d38c65ca32d6f091579c3ad6f
Gary Mannion <gkmannion@hotmail.com>
2018-10-05 14:43:09 +0100

    Missile shoot & destroys after 2 seconds

---
+++ Assets/_Scripts/Destroy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+    public float alive;
+
+	// Use this for initialization
+	void Awake () {
+        Destroy(gameObject, alive);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/_Scripts/projectileController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectileController : MonoBehaviour {
+
+    public float missileSpeed;
+
+    Rigidbody2D RB;
+
+    // Use this for initialization
+    void Awake()
+    {
+        RB = GetComponent<Rigidbody2D>();
+
+        RB.AddForce(new Vector2(1, 0) * missileSpeed, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
