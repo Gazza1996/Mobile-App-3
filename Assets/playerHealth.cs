d993cfe4c4067a960f9166ed70a63a88a96b3a97
Gary Mannion <gkmannion@hotmail.com>
2018-10-15 14:35:28 +0100

    player can now be damaged when coming in contact with an obstacle

---
+++ Assets/playerHealth.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHealth : MonoBehaviour {
+
+    public float fullHealth;
+    public GameObject death;
+
+    float cHealth;
+
+    playerControls controlMovement;
+
+    // Use this for initialization
+    void Start()
+    {
+        cHealth = fullHealth;
+
+        controlMovement = GetComponent<playerControls>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void addDamage(float damage)
+    {
+        if (damage <= 0) return;
+        cHealth -= damage;
+
+        if (cHealth <= 0)
+        {
+            makeDead();
+        }
+    }
+
+    public void makeDead()
+    {
+        Instantiate(death, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
