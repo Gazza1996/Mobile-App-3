71bbb2d209e0046b556504ecf1f2b7b83b38f3ef
Gary Mannion <gkmannion@hotmail.com>
2018-10-18 22:20:46 +0100

    pause menu added

---
+++ Assets/_Scripts/lootHeart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lootHeart : MonoBehaviour {
+
+    public float healthAmt;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            playerHealth health = other.gameObject.GetComponent<playerHealth>();
+            health.addHealth(healthAmt);
+            Destroy(gameObject);
+        }
+    }
+}
