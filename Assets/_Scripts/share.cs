c6687244ac2d9f28ae78cbbe1ee79df021c852bb
Gary Mannion <gkmannion@hotmail.com>
2018-11-06 12:10:07 +0000

    facebook share

---
+++ Assets/_Scripts/share.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class share : MonoBehaviour {
+
+    // facebook variables
+    string AppID = "2861153000577617";
+
+    string Link = "https://google.com";
+
+    string caption = "Share my game";
+
+	// Facebook
+	public void fbShare () {
+        Application.OpenURL("https://www.facebook.com/dialog/feed?" + "app_id=" + AppID + "&link=" + Link + "&caption=" + caption);
+	}
+	
+}
